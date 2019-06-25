75d4a09a37b98fd755c8e2ab141f607a5df6db21
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-25 15:29:32 -0500

    Changes to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
-const app = require('./config.js');
+var express = require('express');
+var bodyParser = require('body-parser');
+// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 
 
+var app = express();
 
-const port = (process.env.PORT || 3000);
+// UNCOMMENT FOR REACT
+ app.use(express.static(__dirname + '/../client/dist'));
 
-app.listen(port, () => {
-  console.log(`Listening on Port ${port}`);
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
 });

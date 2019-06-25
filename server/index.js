76cb1ac83c005954e3f53da701eb45b929b2f5e0
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-25 15:17:57 -0500

    changes to structure

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
 const app = require('./config.js');
 
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 const port = (process.env.PORT || 3000);
 
 app.listen(port, () => {
